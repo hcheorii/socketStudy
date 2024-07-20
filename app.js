5dd20f002856bf729e574888c9628affe272dc04
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-07-21 02:53:48 +0900

    채팅 기능

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require("http");
 const app = express();
 const path = require("path");
 const server = http.createServer(app);
+const moment = require("moment");
 
 const socketIO = require("socket.io");
 
@@ -14,8 +15,13 @@ const PORT = process.env.PORT || 5001;
 
 io.on("connection", (socket) => {
     socket.on("chatting", (data) => {
-        console.log(data);
-        io.emit("chatting", `그래 반가워 ${data}`);
+        const { name, msg } = data;
+
+        io.emit("chatting", {
+            name: name,
+            msg: msg,
+            time: moment(new Date()).format("h:ss A"),
+        });
     });
 });
 server.listen(PORT, () => console.log(`server is running ${PORT}`));
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,7 +1,43 @@
 const socket = io();
-
-socket.emit("chatting", "from front");
-socket.on("chatting", (data) => {
-    console.log(data);
+const nickname = document.querySelector("#nickname");
+const chatList = document.querySelector(".chatting-list");
+const chatInput = document.querySelector("#chatting-input");
+const sendButton = document.querySelector(".send-button");
+const displayContainer = document.querySelector(".display-container");
+sendButton.addEventListener("click", () => {
+    const param = {
+        name: nickname.value,
+        msg: chatInput.value,
+    };
+    socket.emit("chatting", param);
 });
-console.log(socket);
+
+socket.on("chatting", (data) => {
+    const { name, msg, time } = data;
+    const item = new LiModel(name, msg, time);
+    item.makeLi();
+    displayContainer.scrollTo(0, displayContainer.scrollHeight);
+});
+
+function LiModel(name, msg, time) {
+    this.name = name;
+    this.msg = msg;
+    this.time = time;
+
+    this.makeLi = () => {
+        const li = document.createElement("li");
+        li.classList.add(nickname.value === this.name ? "sent" : "received");
+        const dom = `<span class="profile">
+        <span class="user">${this.name}</span>
+        <img
+            class="image"
+            src="https://picsum.photos/50/50"
+            alt="any"
+        />
+    </span>
+    <span class="message">${this.msg}</span>
+    <span class="time">${this.time}</span>`;
+        li.innerHTML = dom;
+        chatList.appendChild(li);
+    };
+}
